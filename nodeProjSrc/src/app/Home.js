12c86f39edd8c13f8acb206b113e3ba86d604e68
Greg DiCristofaro <gdicristofaro@fas.harvard.edu>
2016-06-28 21:32:34 -0400

    fix for home on small screens

--- nodeProjSrc/src/app/Home.js
+++ nodeProjSrc/src/app/Home.js
@@ -6,14 +6,10 @@ export default class Home extends React.Component {
     return (
       <div style={{display: 'block'}}>
         <h1 style={{textAlign: "center"}}>Hello!</h1>
-        <div style={{display: "table-cell", verticalAlign: "top"}}>
-          <img src="profile.jpg" style={{float: "left", marginRight: "10px", height: "200px"}} />
-        </div>
-        <div style={{display: "table-cell", verticalAlign: "top"}}>
-          <p style={{marginTop: 0}}>Hello, and welcome to my website.  I write software and teach music, and you can read more about my work history on my <Link to="/resume">resume</Link>.  Also, you can find some of my <Link to="/projects">software projects</Link> on this website.  All of these projects can also be found on my <a href="https://github.com/gdicristofaro">GitHub account</a> as well.  If you have any questions, feel free to email me at <a href="mailto: gregdicristofaro@gmail.com">gregdicristofaro@gmail.com</a>.</p>
-          <p>Thanks,</p>
-          <p>Greg DiCristofaro</p>
-        </div>
+        <img src="profile.jpg" style={{float: "left", marginRight: "10px", height: "200px"}} />
+        <p style={{marginTop: 0}}>Hello, and welcome to my website.  I write software and teach music, and you can read more about my work history on my <Link to="/resume">resume</Link>.  Also, you can find some of my <Link to="/projects">software projects</Link> on this website.  All of these projects can also be found on my <a href="https://github.com/gdicristofaro">GitHub account</a> as well.  If you have any questions, feel free to email me at <a href="mailto: gregdicristofaro@gmail.com">gregdicristofaro@gmail.com</a>.</p>
+        <p>Thanks,</p>
+        <p>Greg DiCristofaro</p>
       </div>
     );
   }
