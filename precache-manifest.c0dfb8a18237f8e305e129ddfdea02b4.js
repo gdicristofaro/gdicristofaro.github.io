098a7ee847e9534b7e7133a951dc31bb05192db8
Greg DiCristofaro <gdicristofaro@fas.harvard.edu>
2019-01-08 14:59:14 -0500

    Updates

---
+++ precache-manifest.c0dfb8a18237f8e305e129ddfdea02b4.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f7d9db0333edb79c7486",
+    "url": "/static/js/main.f7d9db03.chunk.js"
+  },
+  {
+    "revision": "595ef1fcf8ed72d8d649",
+    "url": "/static/js/1.595ef1fc.chunk.js"
+  },
+  {
+    "revision": "cb39f4be6614f28b0f97aa38cf3ce34e",
+    "url": "/index.html"
+  }
+];
