3f39230466c2554f289bf7124577574507ff8afa
Greg DiCristofaro <gdicristofaro@fas.harvard.edu>
2019-05-21 21:26:36 -0400

    Updates

---
+++ precache-manifest.d6c1c64953590ffa599948388b371f87.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44a648ad37a0bafbdda64d7e1fba6c51",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a881796711f8568d832d",
+    "url": "/static/js/2.8ce346ac.chunk.js"
+  },
+  {
+    "revision": "a236bd063f884e1cf903",
+    "url": "/static/js/main.909e3dd0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3666e8953e772c51d0c6167d789ef172.js"
+  "/precache-manifest.d6c1c64953590ffa599948388b371f87.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
