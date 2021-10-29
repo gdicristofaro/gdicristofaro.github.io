5a5fc55b33ef986b206e06be8e1fd15b59e8a758
Greg DiCristofaro <gregdicristofaro@gmail.com>
2021-10-28 21:04:37 -0400

    website build

---
+++ precache-manifest.e8e4889c303c9707e422370bbef3a1c0.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c39885eea6cbaaf9f269442f8c0264e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a881796711f8568d832d",
+    "url": "/static/js/2.8ce346ac.chunk.js"
+  },
+  {
+    "revision": "095caf7676dd5d98e9aa",
+    "url": "/static/js/main.a6aee3f4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
