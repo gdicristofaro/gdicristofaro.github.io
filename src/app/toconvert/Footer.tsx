66e68f0daab4978c1ee042d151e39a9d32401948
Greg DiCristofaro <gregdicristofaro@gmail.com>
2024-08-31 22:38:54 -0400

    updates

--- src/app/toconvert/Footer.tsx
+++ src/app/toconvert/Footer.tsx
@@ -3,9 +3,11 @@ import React from 'react'
 
 export const footerHeight = 50;
 
-export default () => {
-  <div className='footer' style={{ display: 'table', height: footerHeight + "px", width: '100%' }}>
-    <p style={{ display: 'table-cell', verticalAlign: 'middle' }}>This website brought to you by: <a href="https://facebook.github.io/react/">React</a>, <a href="https://material-ui-next.com/">Material UI</a>, <a href="https://jquery.com/">jQuery</a> & <a href="https://design.google.com/icons/">Google's Material Icons</a>.</p>
+export default () => (
+  <div className="footer">
+    <p className="footer-paragraph">This website brought to you by:
+      <a href="https://facebook.github.io/react/">React</a>, <a href="https://material-ui-next.com/">Material UI</a>,
+      <a href="https://jquery.com/">jQuery</a> & <a href="https://design.google.com/icons/">Google's Material Icons</a>.
+    </p>
   </div>
-
-}
+)
