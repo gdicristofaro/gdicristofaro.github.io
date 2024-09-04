e2f48e30bb8de63d31fa094951e0d28d982aa7d0
Greg DiCristofaro <gregdicristofaro@gmail.com>
2024-09-03 22:41:13 -0400

    updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,11 @@ import React from 'react'
 import Image from 'next/image'
 
 export default () => (
-  <div className="homepage-main page-transition">
+  <div className="homepage-main page-transition page-content">
     <h1 className="homepage-header page-header">Hello!</h1>
     <img className="homepage-profile-picture" src="/img/profile.jpg" alt="my profile picture" />
     <p className="homepage-paragraph">
       Hello, and welcome to my website.
-      I write software and play music.
       You can read more about my work history on my <Link href="/resume">resum&eacute;</Link>.
       Also, you can find some of my <Link href="/projects">software projects</Link> on this website.
       All of these projects can also be found on my <a href="https://github.com/gdicristofaro">GitHub account</a> as well.
