635faf218d7856f7a177c8f327f21f406ed288a2
Greg DiCristofaro <gregdicristofaro@gmail.com>
2024-09-05 22:29:41 -0400

    resume generation

--- build-resume.ts
+++ build-resume.ts
@@ -7,10 +7,14 @@ async function printPDF(dir: string) {
     const resumehtml = await promises.readFile(path.join(dir, "src/model/resumehtml.html"), 'utf-8');
 
     const htmlStr = `<html><head><style>${resumecss}</style></head><body>${resumehtml}</body></html>`;
+    const htmlPath = path.join(dir, "public/resume.html");
+    // can generate html file if below is uncommented
+    //await promises.writeFile(htmlPath, htmlStr);
+
     const browser = await puppeteer.launch({ headless: true });
     const page = await browser.newPage();
     await page.setContent(htmlStr, { waitUntil: 'networkidle0' });
-    const pdf = await page.pdf({ format: 'LETTER', margin: { left: '1in', top: '1in', right: '1in', bottom: '1in' } });
+    const pdf = await page.pdf({ format: 'LETTER', margin: { left: '.5in', top: '.5in', right: '1in', bottom: '1in' } });
 
     const pdfPath = path.join(dir, "public/resume.pdf");
 
