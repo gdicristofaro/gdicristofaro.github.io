ba5ffc0d4302e73652de2fc55772703d79c4b72c
Greg DiCristofaro <gregdicristofaro@gmail.com>
2025-09-07 20:25:59 -0400

    Updates

---
+++ _next/static/uq3YxlPx0yJQq5TWtZ7Cz/_buildManifest.js
@@ -0,0 +1,14 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/22a17206ec64f336.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
