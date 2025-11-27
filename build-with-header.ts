d5abe92cee96ef6d5e6bd015ff259b1417fbbd71
Greg DiCristofaro <gregdicristofaro@gmail.com>
2025-11-26 22:23:44 -0500

    update

--- build-with-header.ts
+++ build-with-header.ts
@@ -1,5 +1,6 @@
 import { promises } from 'fs';
 import path from 'path';
+import { marked } from 'marked';
 
 async function writeHtml(dir: string) {
     const resumecss = await promises.readFile(path.join(dir, "src/styles/resume.css"), 'utf-8');
@@ -9,21 +10,24 @@ async function writeHtml(dir: string) {
 
     const headerContentPath = process.argv[process.argv.length - 2];
     const headeredTitle = process.argv[process.argv.length - 3];
-    const outputFile = process.argv[process.argv.length - 1]
+    const outputFile = process.argv[process.argv.length - 1];
 
     console.log(`writing headered file with title: ${headeredTitle} from content path: ${headerContentPath} to output file: ${outputFile}`);
     const headeredContent = await promises.readFile(headerContentPath, 'utf-8');
+    const headeredHtml = await marked.parse(headeredContent);
+
+    const metaTitle = `Greg DiCristofaro - ${htmlEntities(headeredTitle)}`;
 
     const replacedHeadered = headered
-        .replaceAll("\{\{title\}\}", headeredTitle)
-        .replaceAll("\{\{content\}\}", headeredContent);
+        .replaceAll("\{\{title\}\}", htmlEntities(headeredTitle))
+        .replaceAll("\{\{content\}\}", headeredHtml);
 
     const htmlStr = `
         <!DOCTYPE html>
         <html lang="en">
         <head>
             <meta charset="UTF-8">
-            <title>Greg DiCristofaro - Resume</title>
+            <title>${metaTitle}</title>
             <script src="https://cdn.tailwindcss.com"></script>
             <style>
 
@@ -39,6 +43,14 @@ async function writeHtml(dir: string) {
         </html>`;   
 
     await promises.writeFile(outputFile, htmlStr)
+}
+
+function htmlEntities(str: string): string {
+    return str
+        .replace(/&/g, '&amp;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;')
+        .replace(/"/g, '&quot;');
 }
 
 writeHtml(__dirname).finally(() => console.log("done."));
