dbd66e205bb0e71885c411443edfea914fecd534
Greg DiCristofaro <gregdicristofaro@gmail.com>
2026-05-08 21:20:58 -0400

    updates

--- build-resume.ts
+++ build-resume.ts
@@ -1,12 +1,19 @@
-import * as puppeteer from 'puppeteer';
-import { promises } from 'fs';
-import path from 'path';
+import * as puppeteer from "puppeteer";
+import { promises } from "fs";
+import path from "path";
+import fs from "fs";
 
 async function printPDF(dir: string, outputPath?: string) {
-    const resumecss = await promises.readFile(path.join(dir, "src/styles/resume.css"), 'utf-8');
-    const resumehtml = await promises.readFile(path.join(dir, "src/model/resumehtml.html"), 'utf-8');
+  const resumecss = await promises.readFile(
+    path.join(dir, "src/styles/resume.css"),
+    "utf-8",
+  );
+  const resumehtml = await promises.readFile(
+    path.join(dir, "src/model/resumehtml.html"),
+    "utf-8",
+  );
 
-    const htmlStr = `
+  const htmlStr = `
         <!DOCTYPE html>
         <html lang="en">
         <head>
@@ -20,15 +27,24 @@ async function printPDF(dir: string, outputPath?: string) {
         </body>
         </html>`;
 
-    const browser = await puppeteer.launch({ headless: true });
-    const page = await browser.newPage();
-    await page.setContent(htmlStr, { waitUntil: 'networkidle0' });
-    const pdf = await page.pdf({ format: 'LETTER', margin: { left: '.5in', top: '.5in', right: '.5in', bottom: '.5in' } });
+  const browser = await puppeteer.launch({ headless: true });
+  const page = await browser.newPage();
+  await page.setContent(htmlStr, { waitUntil: "networkidle0" });
+  const pdf = await page.pdf({
+    format: "LETTER",
+    margin: { left: ".5in", top: ".5in", right: ".5in", bottom: ".5in" },
+  });
 
-    const pdfPath = outputPath || path.join(dir, "public/resume.pdf");
+  const pdfPath = outputPath || path.join(dir, "public/resume.pdf");
 
-    await browser.close();
-    await promises.writeFile(pdfPath, Buffer.from(pdf));
+  await browser.close();
+  const parentDir = path.dirname(pdfPath);
+  console.log(`Creating directory ${parentDir} if it does not exist...`);
+  await fs.mkdir(parentDir, { recursive: true }, (err) => {
+    if (err) throw err;
+  });
+  console.log(`Writing PDF to ${pdfPath}...`);
+  await promises.writeFile(pdfPath, Buffer.from(pdf));
 }
 
 const customPath = process.argv[2];
